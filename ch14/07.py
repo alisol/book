391f852a3006d56566804c16240207a2ef0128c5
david <nocaca@daum.net>
2019-10-13 20:38:16 +0900

    Update 07.py
    2019 10 13
    개정판 기준으로 코드를 수정함

--- ch14/07.py
+++ ch14/07.py
@@ -1,14 +1,21 @@
 import pandas_datareader.data as web
-import datetime
+# import datetime
+import fix_yahoo_finance as yf #개정판
 from zipline.api import order, record, symbol
 from zipline.algorithm import TradingAlgorithm
-from zipline.api import set_commission, commission
+from zipline.finance import commission  #개정판
+# from zipline.api import set_commission, commission
 from zipline.utils.factory import create_simulation_parameters
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+import pandas as pd
+pd.set_option('display.float_format', '{:.3f}'.format)   #개정판
 
-start = datetime.datetime(2016, 1, 1)
-end = datetime.datetime(2016, 1, 31)
-data = web.DataReader("078930.KS", "yahoo", start, end)
+yf.pdr_override()  #개정판
+
+# start = datetime.datetime(2016, 1, 1)
+# end = datetime.datetime(2016, 1, 31)
+# data = web.DataReader("078930.KS", "yahoo", start, end) #개정판
+data = web.get_data_yahoo("078930.KS", start="2019-01-02", end="2019-01-25") #개정판
 
 data = data[['Adj Close']]
 data.columns = ['GS']
