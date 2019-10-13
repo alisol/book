a296706cdbdcac70b2f92ae44a10da4304a51f64
david <nocaca@daum.net>
2019-10-13 20:53:48 +0900

    Update 13.py
    개정판 책하고 코드가 약간 다름. 왜 수정한건지 모르겠음

--- ch15/13.py
+++ ch15/13.py
@@ -6,13 +6,14 @@ import matplotlib.ticker as ticker
 
 start = datetime.datetime(2016, 3, 1)
 end = datetime.datetime(2016, 3, 31)
+
 skhynix = web.DataReader("000660.KS", "yahoo", start, end)
-#skhynix = skhynix[skhynix['Volume'] > 0]
+#skhynix = skhynix[skhynix['Volume'] > 0] # 개정판에는 이 부분이 실행되도록 설정됨. 왜 주석처리한건가?
 
 fig = plt.figure(figsize=(12, 8))
 ax = fig.add_subplot(111)
-
 day_list = range(len(skhynix))
+
 name_list = []
 for day in skhynix.index:
     name_list.append(day.strftime('%d'))
