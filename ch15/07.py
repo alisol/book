1cba1bf84f19bacde14bf4d8964ccd366943878b
david <nocaca@daum.net>
2019-10-13 20:45:44 +0900

    Update 07.py
    2019 10 13
    개정판 코드 기준으로 수정함

--- ch15/07.py
+++ ch15/07.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 lg = web.DataReader("066570.KS", "yahoo")
 samsung = web.DataReader("005930.KS", "yahoo")
 
-plt.plot(lg.index, lg['Adj Close'], label='LG Electronics')
-plt.plot(samsung.index, samsung['Adj Close'], label='Samsung Electronics')
+# plt.plot(lg.index, lg['Adj Close'], label='LG Electronics')
+# plt.plot(samsung.index, samsung['Adj Close'], label='Samsung Electronics')
+plt.plot(lg.index, lg['Close'], label='LG Electronics') # 개정판 코드
+plt.plot(samsung.index, samsung['Close'], label='Samsung Electronics') # 개정판 코드
 
 plt.legend(loc='upper left')
 plt.show()
