5317e2e9545e4bb57d806764cdb2b37c5ed18631
david <nocaca@daum.net>
2019-10-13 20:55:23 +0900

    Update 14.py
    2019 10 13
    개정판 기준으로 주석만 넣음

--- ch15/14.py
+++ ch15/14.py
@@ -7,7 +7,7 @@ import matplotlib.ticker as ticker
 start = datetime.datetime(2016, 3, 1)
 end = datetime.datetime(2016, 3, 31)
 skhynix = web.DataReader("000660.KS", "yahoo", start, end)
-#skhynix = skhynix[skhynix['Volume'] > 0]
+#skhynix = skhynix[skhynix['Volume'] > 0]   #개정판 책에는 이 부분이 주석처리가 아님. 
 
 fig = plt.figure(figsize=(12, 8))
 ax = fig.add_subplot(111)
@@ -23,5 +23,5 @@ ax.xaxis.set_major_locator(ticker.FixedLocator(day_list))
 ax.xaxis.set_major_formatter(ticker.FixedFormatter(name_list))
 
 mpl_finance.candlestick2_ohlc(ax, skhynix['Open'], skhynix['High'], skhynix['Low'], skhynix['Close'], width=0.5, colorup='r', colordown='b')
-plt.grid()
+plt.grid()      # 개정판 책에는 이 부분이 없음. 
 plt.show()
